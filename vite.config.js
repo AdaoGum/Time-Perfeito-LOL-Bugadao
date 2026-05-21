0fd536313c84010e57875951e396b6565d23f133
AdaoGum <adaojmsantos@gmail.com>
2026-05-21 01:29:43 -0300

    Adição da action do NPM tentativa 2

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import tailwindcss from '@tailwindcss/vite';
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    vue()
-  ],
-  // IMPORTANTE: Coloque exatamente o nome do seu repositório aqui entre as barras
+  plugins: [vue()],
   base: '/Time-Perfeito-LOL-Bugadao/', 
 })
